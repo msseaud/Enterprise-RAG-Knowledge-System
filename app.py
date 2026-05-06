7bf7d70ef4a11134e8ecf0001780917b314130a0
msseaud <jimibukexielu@gmail.com>
2026-05-06 19:16:37 +0800

    init project structure

---
+++ app.py
@@ -0,0 +1,10 @@
+from fastapi import FastAPI
+from api.routes import router
+
+app = FastAPI()
+
+app.include_router(router)
+
+@app.get("/")
+def root():
+    return {"message": "RAG system running"}
