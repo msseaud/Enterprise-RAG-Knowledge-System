9183821be1b669047203b6e13a04d8a668ee239e
msseaud <jimibukexielu@gmail.com>
2026-05-06 19:54:52 +0800

    Delete api/core/memory/vector_store.py

--- api/core/memory/vector_store.py
+++
@@ -1,9 +0,0 @@
-class VectorStore:
-    def __init__(self):
-        self.data = []
-
-    def add(self, items):
-        self.data.extend(items)
-
-    def search(self, query):
-        return self.data[:3]
